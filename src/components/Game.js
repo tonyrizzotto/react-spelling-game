c491109b40cb1ea7c7a52fcfe534a13100158ab1
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-08-09 17:28:41 -0400

    Update: Correctly implemented input validation and color feedback
    ToDo: need to restrict the input to either only the appropriate number of characters OR display a message saying 'too many characters'.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,53 +1,73 @@
 import React, { useState, useEffect } from 'react';
+import './Game.css';
 
-const GameDisplay = ({ data }) => {
-  //initialize state for selected data
+const Game = ({ data }) => {
+  //initialize state for selected data: selecting word length for later version
   const [gameData, setGameData] = useState(data.threeLetterWords);
-  //initialize state for difficulty
-  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+
+  //set the displayed question to a random index in the game data
+  const [currentQuestion, setCurrentQuestion] = useState(
+    gameData[Math.floor(Math.random() * gameData.length)]
+  );
+
   //initilize state for a controlled input
   const [input, setInput] = useState('');
 
-  //Helper function to change difficulty
-  const setDifficulty = (event) => {
-    setSelectedDifficulty(event.target.value);
-    if (selectedDifficulty === '3') {
-      setGameData(data.threeLetterWords);
-    } else {
-      setGameData(data.fourLetterWords);
-    }
-  };
+  //side effect to monitor current word
+  useEffect(() => {
+    displayWord(currentQuestion.name, input);
+  }, [input, currentQuestion]);
+
+  //write a function to place the word into the div
+  function displayWord(word, input) {
+    //this function should place the word and the HTML into the div
+    let spanDiv = document.getElementById('word');
+
+    //clear the div to avoid duplicate placements
+    spanDiv.innerHTML = '';
+
+    //break the word into an array
+    let brokenWord = word.split('');
+    // store the input into an array
+    let inputArray = input.split('');
+
+    // make sure input doesn't go longer than the word
+
+    //display each word as an individual span
+    brokenWord.forEach((letter) => {
+      //create a span element
+      let span = document.createElement('SPAN');
+
+      // validate each letter from the input to determine color
+      inputArray.forEach((i) => {
+        if (
+          i === letter &&
+          inputArray.indexOf(i) === brokenWord.indexOf(letter)
+        ) {
+          span.classList.add('correct');
+        } else if (
+          i !== letter &&
+          inputArray.indexOf(i) === brokenWord.indexOf(letter)
+        ) {
+          span.classList.add('wrong');
+        }
+      });
+
+      //display each letter in the span
+      span.innerText = letter;
+      //push the letter to the DIV
+      spanDiv.appendChild(span);
+    });
+  }
 
   return (
     <div className="ui vertically divided grid">
-      <div style={{ textAlign: 'center' }}>
-        <span>
-          Set Difficulty:
-          <button
-            className="ui button"
-            id="three"
-            value="3"
-            onClick={setDifficulty}
-          >
-            3-Letter Words
-          </button>
-          <button
-            className="ui button"
-            id="four"
-            value="4"
-            onClick={setDifficulty}
-          >
-            4-Letter Words
-          </button>
-        </span>
-        <div>{selectedDifficulty}</div>
-      </div>
       <div className="two column row">
         <div className="column">
-          <img />
+          <img src={currentQuestion.picture} alt={currentQuestion.name} />
         </div>
-        <div className="column">
-          <div className="segment">{gameData[0].name}</div>
+        <div className="column input-column">
+          <div id="word" className="segment word-to-spell"></div>
           <div className="segment">
             <div className="ui massive input">
               <input
@@ -57,12 +77,16 @@ const GameDisplay = ({ data }) => {
                   setInput(e.target.value);
                 }}
               />
+              <button className="button">Submit</button>
             </div>
           </div>
+        </div>
+        <div className="column">
+          <div className="segment description">{currentQuestion.desc}</div>
         </div>
       </div>
     </div>
   );
 };
 
-export default GameDisplay;
+export default Game;
